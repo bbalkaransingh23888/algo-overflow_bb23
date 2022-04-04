6c47322bd192a28a4d738994bab40f6cb1125290
Brandon Balkaransingh <brandonbalkaransingh@Brandons-MacBook-Pro.local>
2022-04-04 15:39:38 -0400

    brute force solution for two sum complete

---
+++ precourse_mastering-the-coding-interview-big-tech/precourse-solutions.py
@@ -0,0 +1,23 @@
+### ARRAYS: EASY - TWO SUM ###
+
+##BRUTE FORCE SOLUTION##
+def two_sum(nums, t):
+    # p1, or i, compares each number for the length of the array, 
+    # which is derived from length - 1 (arrays start at 0, 
+    # so len(nums) - 1 starts the count from the beginning)
+    for i in range(len(nums) -1):
+        # p2, or j, compares each number that comes after i, or the remaining 
+        # length of the array. i+1 starts the count at the second element while
+        # len(nums) has the count include every number except the element in i 
+        # and the prior elements where applicable
+        for j in range(i+1, len(nums)):
+            # if the two pointers eventually add up to the target, 
+            if nums[i] + nums[j] == t:
+                print(i, j)
+    print("null")
+
+two_sum([1,3,7,9,2], 11)
+two_sum([2], 2)
+two_sum([], 1)
+two_sum([2,3], 5)
+two_sum([1,2,3], 6)
